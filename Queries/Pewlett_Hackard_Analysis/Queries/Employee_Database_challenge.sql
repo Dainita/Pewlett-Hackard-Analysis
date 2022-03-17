3d97b83819b0892b4d73cac1298e73f2313752b7
Dainita <97318406+Dainita@users.noreply.github.com>
2022-03-16 21:51:35 -0400

    Update Employee_Database_challenge.sql

--- Queries/Pewlett_Hackard_Analysis/Queries/Employee_Database_challenge.sql
+++ Queries/Pewlett_Hackard_Analysis/Queries/Employee_Database_challenge.sql
@@ -28,3 +28,19 @@ ORDER BY COUNT(ut.title) DESC;
 
 SELECT * FROM retiring_titles
 
+SELECT DISTINCT ON (emp_no) e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+d.from_date,
+d.to_date,
+ttl.title
+INTO mentorship_eligibility
+FROM employees AS e
+INNER JOIN dept_emp AS d
+ON (e.emp_no=d.emp_no)
+INNER JOIN titles AS ttl
+ON (e.emp_no=ttl.emp_no)
+WHERE (d.to_date = '9999-01-01')
+	AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
