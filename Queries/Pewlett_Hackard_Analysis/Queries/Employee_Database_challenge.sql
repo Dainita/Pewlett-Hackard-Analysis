c6808074c2304ea07f1fb41a9467b9c17b4efb4b
Dainita <97318406+Dainita@users.noreply.github.com>
2022-04-03 11:49:07 -0400

    Update Employee_Database_challenge.sql

--- Queries/Pewlett_Hackard_Analysis/Queries/Employee_Database_challenge.sql
+++ Queries/Pewlett_Hackard_Analysis/Queries/Employee_Database_challenge.sql
@@ -9,7 +9,8 @@ INTO retirement_titles_table
 FROM employees AS e
 INNER JOIN titles AS ttl
 ON (e.emp_no=ttl.emp_no)
-WHERE e.birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+WHERE e.birth_date BETWEEN '1952-01-01' AND '1955-12-31'
+ORDER BY e.emp_no;
 
 -- Create table of unique titles by removing duplicates
 SELECT DISTINCT ON (emp_no) r.emp_no,
